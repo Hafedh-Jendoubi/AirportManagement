d94a99280c24ec4969b55754af0ce7b44b28cb2d
Hafedh-Jendoubi <hafeudh8@gmail.com>
2025-02-06 21:54:49 +0100

    Creating Entities

---
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public enum PlaneType {
+        Boeing,
+        Airbus
+    }
+
+    public class Plane
+    {
+        //Property
+        public int Capacity { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public int PlaneId { get; set; }
+        public PlaneType PlaneType { get; set; }
+    }
+}
